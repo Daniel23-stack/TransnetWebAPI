aa2e40475c4b4c0acba4f7db1bd70baa3b2ea7d5
daniel <daniel.qhobosheane@providencesoft.com>
2023-01-26 16:18:03 +0200

    Adding Front-End

--- TransnetWebAPI/Controllers/UsersController.cs
+++ TransnetWebAPI/Controllers/UsersController.cs
@@ -1,7 +1,7 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using TransnetWebAPI.Data;
 using TransnetWebAPI.Models;
-using System.Linq;
+using Microsoft.EntityFrameworkCore;
 
 namespace TransnetWebAPI.Controllers;
 
@@ -18,15 +18,15 @@ public class UsersController : ControllerBase
     }
 
     [HttpGet]
-    public ActionResult<IEnumerable<AppUser>> GetUsers()
+    public async Task<ActionResult<IEnumerable<AppUser>>> GetUsers()
     {
-        return _context.Users.ToList();
+        return await _context.Users.ToListAsync();
 
     }
     [HttpGet("{id}")]
-    public ActionResult<AppUser> GetUser(int id)
+    public async Task<ActionResult<AppUser>> GetUser(int id)
     {
-        return _context.Users.Find(id);
+        return await _context.Users.FindAsync(id);
 
     }
 }
